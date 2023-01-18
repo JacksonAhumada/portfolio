918921bfd483f178c94a0dc68ff23a994927a14c
JacksonAhumada <jacksonahumada@gmail.com>
2023-01-17 21:04:05 -0700

    Hero img created. header finished and styled.

---
+++ src/components/HeroImage.js
@@ -0,0 +1,29 @@
+import "./HeroImageStyles.css";
+
+import React from "react";
+import IntroImg from "../assets/intro-bg.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImage = () => {
+  return (
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={IntroImg} alt="IntroImg" />
+      </div>
+      <div className="content">
+        <p>Hi I'm Jackson Ahumada</p>
+        <h1>Fullstack Developer</h1>
+        <div>
+          <Link to="/project" className="btn">
+            PROJECTS
+          </Link>
+          <Link to="/contact" className="btn btn-light">
+            CONTACT
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImage;
